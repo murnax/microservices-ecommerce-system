1730715fb296496985b3669d2a04f8632b203856
Suratin Pattanawongthai <suratin.p@gobike.asia>
2018-07-08 20:15:51 +0700

    update

---
+++ consumerDemo.js
@@ -0,0 +1,30 @@
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient({kafkaHost: 'kafka:9092'}),
+    consumer = new Consumer(
+        client,
+        [
+            { topic: 'test_topic', partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );
+    client.connect();
+    // console.log(client);
+
+    client.on('ready', function (err){
+        console.log('client ready', err);
+    });
+
+    client.on('error', function (err){
+        console.log('client error', err);
+    });
+
+consumer.on('message', function (message) {
+    console.log(message);
+});
+
+consumer.on('error', function (err){
+    console.log('consumer error', err);
+});
