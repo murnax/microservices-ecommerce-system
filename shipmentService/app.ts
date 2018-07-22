48f3a1ca9611f7f7db7c8161269242657a660036
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-22 14:06:24 +0700

    add create shipment endpoint to shipment service

--- shipmentService/app.ts
+++ shipmentService/app.ts
@@ -1,4 +1,5 @@
 import * as kafka from 'kafka-node';
+import * as express from 'express';
 
 const client = new kafka.KafkaClient({ kafkaHost: 'kafka:9092' })
 const producer = new kafka.Producer(client);
@@ -8,33 +9,19 @@ const consumer = new kafka.Consumer(
         { topic: 'ordering', partition: 0 }
     ],
     {
-        autoCommit: false
+        groupId: 'shipmentService',
+        autoCommit: true
     }
 );
 
-client.connect();
-
-client.on('ready', function (err) {
-    console.log('client ready...');
-});
-
-client.on('error', function (err) {
-    console.log('clieasfnt error', err);
-});
-
-consumer.on('message', message => {
-    const eventPayload = JSON.parse(message.value.toString());
-    if (eventPayload.type !== 'order_paid') {
-        return;
-    }
-
+const app = express();
+app.post('/shipments/', (req, res) => {
     const shipment = {
         shipmentId: 1,
         orders: [
             487
         ]
     };
-
 
     const event = {
         event: 'shipment',
@@ -48,11 +35,35 @@ consumer.on('message', message => {
         messages: JSON.stringify(event)
     }];
     producer.send(payload, (err, data) => {
-        console.log('sent event', payload);
+        console.log('sent shipment.shipment_created event');
     });
+
+    res.json('ok');
+});
+
+client.on('ready', () => {
+    console.log('client ready');
+});
+
+client.on('error', function (err) {
+    console.log('clieasfnt error', err);
+});
+
+consumer.on('message', message => {
+    const eventPayload = JSON.parse(message.value.toString());
+    if (eventPayload.type === 'order_created') {
+        console.log('save order into shipment service');
+        return;
+    } else if (eventPayload.type === 'order_paid') {
+        console.log('update order status to shippable');
+        return;
+    }
 });
 
 consumer.on('error', function (err) {
     console.log('consumer error', err);
 });
 
+app.listen(process.env.API_PORT, () => {
+    console.log(`Rest service is now running on port ${process.env.API_PORT}`);
+});
