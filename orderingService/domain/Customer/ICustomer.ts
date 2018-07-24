27121839b0609487c8e3d913ecb1746826ac1c33
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-24 23:48:59 +0700

    update customer repository interface

--- orderingService/domain/Customer/ICustomer.ts
+++ orderingService/domain/Customer/ICustomer.ts
@@ -1,3 +1,7 @@
+import Customer from './Customer';
+import CustomerId from "./CustomerId";
+
 export default interface ICustomerRepository {
     
+    getById(customerId: CustomerId) : Customer;
 }
