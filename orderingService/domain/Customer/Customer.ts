a504bf101954b415775f409eb36320c0a796375a
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-24 23:48:46 +0700

    update customer model

--- orderingService/domain/Customer/Customer.ts
+++ orderingService/domain/Customer/Customer.ts
@@ -1,3 +1,11 @@
+import CustomerId from "./CustomerId";
+
 export default class Customer {
-    
+    readonly customerId: CustomerId;
+    readonly customerName: String;
+
+    constructor(customerId: CustomerId, customerName: String) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
 }
