f9a53afec33dcbadc6e8bc104310680f88497ebf
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-27 16:32:41 +0700

    add order status to order model

--- orderingService/domain/Order/Order.ts
+++ orderingService/domain/Order/Order.ts
@@ -2,14 +2,19 @@ import OrderId from './OrderId';
 import LineItem from './LineItem';
 import Customer from '../Customer/Customer';
 
+
+
 export default class Order {
     orderId: OrderId;
+    orderNumber: String;
     customer: Customer;
     lineItems: Array<LineItem>;
+    status: number;
     
-    constructor(orderId: OrderId, customer: Customer, lineItems: Array<LineItem>) {
+    constructor(orderId: OrderId, customer: Customer, lineItems: Array<LineItem>, status: number) {
         this.orderId = orderId;
         this.customer = customer;
         this.lineItems = lineItems;
+        this.status = status;
     }
 }
