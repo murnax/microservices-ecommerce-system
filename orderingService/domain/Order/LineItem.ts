8ec4f31b2c1a187fdfa3831d7b1bd20436f96de8
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-27 16:32:14 +0700

    update line item constructor

--- orderingService/domain/Order/LineItem.ts
+++ orderingService/domain/Order/LineItem.ts
@@ -1,3 +1,14 @@
 export default class LineItem {
     
+    readonly productId: any;
+    readonly quantity: number;
+    readonly unitPrice: number;
+    readonly total: number;
+
+    constructor(productId: any, quantity: number, unitPrice: number) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.total = quantity * unitPrice;
+    }
 }
