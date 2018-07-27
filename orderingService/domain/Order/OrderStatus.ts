c979f0c8fde824764fb501d9c6234a72eb213de3
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-27 16:31:19 +0700

    add order status module

---
+++ orderingService/domain/Order/OrderStatus.ts
@@ -0,0 +1,6 @@
+export default class OrderStatus {
+    static readonly PENDING: number = 1;
+    static readonly COMPLETED: number = 2;
+    static readonly CANCELLED: number = 3;
+    static readonly DECLINED: number = 4;
+}
