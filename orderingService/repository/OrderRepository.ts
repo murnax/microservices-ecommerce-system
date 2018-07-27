81c6a95dc5ee3914cc98108683da9b07f761465c
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-27 19:47:44 +0700

    add mongo to order repository

--- orderingService/repository/OrderRepository.ts
+++ orderingService/repository/OrderRepository.ts
@@ -1,6 +1,20 @@
+import mongoose = require('mongoose');
+const Mixed = mongoose.Schema.Types.Mixed;
+
 import IOrderRepository from "domain/Order/IOrderRepository";
 import Order from "domain/Order/Order";
 import OrderId from "domain/Order/OrderId";
+
+const OrderSchema = new mongoose.Schema({
+    orderId: String,
+    customerContactInfo: Mixed,
+    lineItems: [ Mixed ],
+    status: { type: Number, min: 1 },
+    subTotal: { type: Number, min: 0 },
+    totalQuantity: { type: Number, min: 1 },
+    deliveryAddress: String
+});
+const OrderModel = mongoose.model('Order', OrderSchema);
 
 export default class OrderRepository implements IOrderRepository {
     
@@ -9,6 +23,14 @@ export default class OrderRepository implements IOrderRepository {
     }
 
     create(order: Order) : void {
-        console.log('save order to persistent storage');
+        try {
+            console.log('save order to persistent storage.');
+            new OrderModel(order.toJSON()).save()
+                .then(data => {
+                    console.log(data);
+                });
+        } catch(error) {
+            console.log(error);
+        }
     }
 }
