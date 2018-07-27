8a886df44d8050baae9ec2e21c7b7acbe354249a
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-27 16:39:03 +0700

    add sub total and total quantity to order model

--- orderingService/domain/Order/Order.ts
+++ orderingService/domain/Order/Order.ts
@@ -10,11 +10,15 @@ export default class Order {
     customer: Customer;
     lineItems: Array<LineItem>;
     status: number;
+    subTotal: number;
+    totalQuantity: number;
     
     constructor(orderId: OrderId, customer: Customer, lineItems: Array<LineItem>, status: number) {
         this.orderId = orderId;
         this.customer = customer;
         this.lineItems = lineItems;
         this.status = status;
+        this.subTotal = lineItems.reduce((a, b) => { return a + b.total; }, 0);
+        this.totalQuantity = lineItems.reduce((a, b) => { return a + b.quantity; }, 0);
     }
 }
