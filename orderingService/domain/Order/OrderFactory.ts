1d0c0dd369678b61be4ddb5cfe324a4fdcd6762c
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-29 00:38:54 +0700

    update reconstitute method - convert raw order object to order model (should later rename it to orderDTO with DTO class

--- orderingService/domain/Order/OrderFactory.ts
+++ orderingService/domain/Order/OrderFactory.ts
@@ -1,4 +1,8 @@
-import Order from './Order';
+import Order from "@root/domain/Order/Order";
+import LineItem from "@root/domain/Order/LineItem";
+import OrderId from '@root/domain/Order/OrderId';
+import OrderStatus from '@root/domain/Order/OrderStatus';
+import CustomerContactInfo from '@root/domain/Order/CustomerContactInfo';
 
 export default class OrderFactory {
 
@@ -9,4 +13,18 @@ export default class OrderFactory {
     create() : Order {
         return;
     }
-}
+
+    static reconstitute(rawOrder: any) : Order {
+        const orderId = new OrderId(rawOrder.orderId);
+
+        const { fullname, email, phoneNumber } = rawOrder.customerContactInfo;
+        const customerContactInfo = new CustomerContactInfo(fullname, email, phoneNumber);
+        
+        const lineItems: Array<LineItem> = rawOrder.lineItems.map((lineItem: any, index: number) => {
+            return new LineItem(lineItem.productId, lineItem.quantity, lineItem.unitPrice);
+        });
+        
+        const order = new Order(orderId, customerContactInfo, lineItems, rawOrder.status, rawOrder.deliveryAddress, rawOrder.subTotal, rawOrder.totalQuantity);
+        return order;
+    }
+} 
