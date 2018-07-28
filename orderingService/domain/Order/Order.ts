86dda17bb9a44d6ad061db482f8770571c771ffa
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-07-29 00:39:45 +0700

    update order model - move calculation logics to it own method - add pay method

--- orderingService/domain/Order/Order.ts
+++ orderingService/domain/Order/Order.ts
@@ -1,4 +1,5 @@
 import OrderId from './OrderId';
+import OrderStatus from './OrderStatus';
 import LineItem from './LineItem';
 import Customer from '../Customer/Customer';
 import CustomerContactInfo from './CustomerContactInfo';
@@ -12,14 +13,27 @@ export default class Order {
     totalQuantity: Number;
     deliveryAddress: any;
     
-    constructor(orderId: OrderId, customerContactInfo: CustomerContactInfo, lineItems: Array<LineItem>, status: Number, deliveryAddress: any) {
+    constructor(orderId: OrderId, customerContactInfo: CustomerContactInfo, lineItems: Array<LineItem>, 
+        status: Number, deliveryAddress: any, subTotal?: Number, totalQuantity?: Number) {
         this.orderId = orderId;
         this.customerContactInfo = customerContactInfo;
         this.lineItems = lineItems;
         this.status = status;
-        this.subTotal = lineItems.reduce((a, b) => { return a + b.total; }, 0);
-        this.totalQuantity = lineItems.reduce((a, b) => { return a + b.quantity; }, 0);
         this.deliveryAddress = deliveryAddress;
+        this.subTotal = subTotal;
+        this.totalQuantity = totalQuantity;
+    }
+
+    calculate() {
+        this.subTotal = this.lineItems.reduce((a, b) => { return a + b.total; }, 0);
+        this.totalQuantity = this.lineItems.reduce((a, b) => { return a + b.quantity; }, 0);
+    }
+
+    pay() {
+        if (this.status !== OrderStatus.PENDING) {
+            // throw error
+        }
+        this.status = OrderStatus.PAID;
     }
 
     toJSON() {
