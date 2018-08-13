88380cfed01e8e04fdbec391f05c28c341e6c802
Suratin Pattanawongthai <suratin.p@gobike.asia>
2018-08-13 13:18:31 +0700

    update OrderQueries model - remove namespace - add view models inside this module - add two methods for returning order and list of orders

--- orderingService/queries/OrderQueries.ts
+++ orderingService/queries/OrderQueries.ts
@@ -1,9 +1,45 @@
-namespace Services.Ordering.Queries {
+import mongoose = require('mongoose');
+const OrderModel = mongoose.model('Order');
 
-    export class OrderQueries {
+class Order {
+    public orderId: string;
+    public lineItems: Array<LineItem>;
+    public status: number;
 
-        async getOrder(orderId: any) : Promise<Order> {
-            return;
-        }
+}
+
+class LineItem {
+    public productId: string;
+    public productName: string;
+    public quantity: number;
+    public unitPrice: number;
+}
+
+class OrderSummary {
+    public orderId: string;
+    public status: number;
+    public subTotal: number;
+    public totalQuantity: number;
+
+    constructor(orderId: string, status: number, subTotal: number, totalQuantity: number) {
+        this.orderId = orderId;
+        this.status = status;
+        this.subTotal = subTotal;
+        this.totalQuantity = totalQuantity;
+    }
+}
+
+export default class OrderQueries {
+
+    async getOrder(orderId: any): Promise<Order> {
+        const order = await OrderModel.findOne({ orderId });
+        return order;
+        // return new Order(order);
+    }
+
+    async getOrders() : Promise<Array<OrderSummary>> {
+        return (await OrderModel.find()).map(order => {
+            return new OrderSummary(order.orderId, order.status, order.subTotal, order.totalQuantity);
+        });
     }
 }
