80536fb40516e6251cea9b9a3b1f7e77345450fe
Suratin Pattanawongthai <suratin.elec@gmail.com>
2018-08-30 00:31:15 +0700

    move three more ordering endpoints to routing & controller

--- orderingService/controller/order/OrderController.ts
+++ orderingService/controller/order/OrderController.ts
@@ -12,7 +12,7 @@ const orderQueryHandler = new OrderQueryHandler();
 
 export default class OrderController {
 
-    async createOrder(req, res) : void {
+    async createOrder(req, res): void {
         const orderDetail = req.body;
         const userId = 1;
         const command = {
@@ -25,12 +25,11 @@ export default class OrderController {
 
         const order = await orderCommandHandler.createOrder(command);
 
-        const event = {
-            event: 'ordering',
-            type: 'order_created',
-            ...order
-        };
-
+        // const event = {
+        //     event: 'ordering',
+        //     type: 'order_created',
+        //     ...order
+        // };
         // const payload = [{
         //     topic: event.event,
         //     key: order.orderId,
@@ -43,11 +42,63 @@ export default class OrderController {
         res.json(order);
     }
 
-    paidOrder(req, res) {
+    async paidOrder(req, res) {
+        const { orderId } = req.params;
+
+        const command = {
+            name: 'payOrder',
+            payload: {
+                userId: 1,
+                orderId
+            }
+        };
+
+        await orderCommandHandler.payOrder(command);
+
+        // const event = {
+        //     event: 'ordering',
+        //     type: 'order_paid',
+        //     orderId
+        // };
+        // const payload = [{
+        //     topic: event.event,
+        //     key: orderId,
+        //     messages: JSON.stringify(event)
+        // }];
+        // producer.send(payload, (err, data) => {
+        //     console.log('sent ordering.order_paid event');
+        // });
+
+        res.json('ok');
     }
 
-    confirmOrder() {
+    async confirmOrder(req, res) {
+        const { orderId } = req.params;
 
+        const command = {
+            name: 'confirmOrder',
+            payload: {
+                userId: 1,
+                orderId
+            }
+        };
+        await orderCommandHandler.confirmOrder(command);
+
+        // const event = {
+        //     event: 'ordering',
+        //     type: 'order_confirmed',
+        //     orderId
+        // };
+        // const payload = [{
+        //     topic: event.event,
+        //     key: orderId,
+        //     messages: JSON.stringify(event)
+        // }];
+        // producer.send(payload, (err, data) => {
+        //     console.log('sent ordering.order_paid event');
+        // });
+
+        res.json('ok');
     }
 
     async getOrders(req, res) {
--- orderingService/route.ts
+++ orderingService/route.ts
@@ -3,6 +3,10 @@ const orderController = new OrderController();
 
 module.exports = function(app) {
 
+    app.post('/orders', orderController.createOrder);
+    app.post('/orders/:orderId/paid', orderController.paidOrder);
+    app.post('/orders/:orderId/confirm', orderController.confirmOrder);
+
     app.get('/orders', orderController.getOrders);
     app.get('/orders/:orderId', orderController.getOrder);
 }
