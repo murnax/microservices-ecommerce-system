02607d2a2671aa5ea78faf4b0cda25d74507e605
Suratin Pattanawongthai <suratin.p@gobike.asia>
2018-12-01 18:39:44 +0700

    order entity reconstitution no longer done inside repository

--- orderingService/infrastructure/repository/OrderRepository.ts
+++ orderingService/infrastructure/repository/OrderRepository.ts
@@ -4,7 +4,6 @@ const Mixed = mongoose.Schema.Types.Mixed;
 import IOrderRepository from "@root/domain/Order/IOrderRepository";
 import Order from "@root/domain/Order/Order";
 import OrderId from "@root/domain/Order/OrderId";
-import OrderFactory from '@root/domain/Order/OrderFactory';
 
 const OrderSchema = new mongoose.Schema({
     orderId: String,
@@ -19,15 +18,15 @@ const OrderModel = mongoose.model('Order', OrderSchema);
 
 export default class OrderRepository implements IOrderRepository {
     
-    async getById(orderId: OrderId) : Promise<Order> {
+    async getById(orderId: OrderId) : Promise<Object> {
         const order = await OrderModel.findOne({ orderId })
         if (!order) {
             return null;
         }
-        return OrderFactory.reconstitute(order);
+        return order;
     }
 
-    create(order: Order) : void {
+    create(order: Order) {
         new OrderModel(order.toJSON()).save();
     }
 
